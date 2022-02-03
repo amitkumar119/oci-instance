0ca50ee576d80d23b99fe6e2da8b6ace2dc325fa
amitkumar119 <35453180+amitkumar119@users.noreply.github.com>
2022-02-03 13:01:07 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,22 +1,22 @@
-variable "count_of_instances"{
+
+provider "oci" {
+   region = "us-ashburn-1"
+   //alias = "phx"
+   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaauuao4pnrffvzgxylvbobagcyo35qsu6z4bqdvjcla4czevhriqvq"
+   user_ocid = "ocid1.user.oc1..aaaaaaaawimuaayeafktaqctwt4twmobbka5awtsepkd4rvss4mtvykxg3pa"
+   fingerprint = "9c:ed:78:5d:da:35:17:05:0a:6b:93:be:b0:84:38:5c"
+   private_key_path = "./amitTestPrivate.pem"
 }
 
-variable "private_key"{
-}
-provider "oci" {
-  tenancy_ocid         = "ocid1.tenancy.oc1..aaaaaaaauuao4pnrffvzgxylvbobagcyo35qsu6z4bqdvjcla4czevhriqvq"
-  user_ocid            = "ocid1.user.oc1..aaaaaaaawimuaayeafktaqctwt4twmobbka5awtsepkd4rvss4mtvykxg3pa"
-  fingerprint          = "4d:27:1c:92:29:78:bc:0e:a7:45:ba:5b:f0:50:98:25"
-  #private_key_path     = "/Users/usman/Desktop/Terraform/Practise-2/mahammad.thotti-11-09-06-09.pem"
-  private_key     = var.private_key
-  region               = "us-ashburn-1"
-  disable_auto_retries = false
-}
-resource "oci_core_instance" "testhost" {
+
+resource "oci_core_instance" "testhostAmit" {
   # Required
-  count = var.count_of_instances
+  count = 1
   availability_domain = "ubQd:US-ASHBURN-AD-2"
-  compartment_id      = "ocid1.compartment.oc1..aaaaaaaaywbpujwoxt7wpxvxawpv2xneun36ccmwk4baal7gpjmsswosmrwq"
+  // For Compartment = AmitChildCompartment 
+  //compartment_id      = "ocid1.compartment.oc1..aaaaaaaaywbpujwoxt7wpxvxawpv2xneun36ccmwk4baal7gpjmsswosmrwq"
+  // For Compartment = oci-intelibliss-test
+  compartment_id =  "ocid1.compartment.oc1..aaaaaaaammdmuwe7hgei2iu4lar6rtg7o4gmvnp572lfxvgwo5xky4z6ajwa"
   shape               = "VM.Standard.E2.1.Micro"
   shape_config {
     memory_in_gbs = 1
@@ -24,14 +24,16 @@ resource "oci_core_instance" "testhost" {
   }
   source_details {
     source_id   = "ocid1.image.oc1.iad.aaaaaaaagubx53kzend5acdvvayliuna2fs623ytlwalehfte7z2zdq7f6ya"
-    #source_id = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
     source_type = "image"
   }
   # Optional
-  display_name = "testhost${count.index}"
+  display_name = "testhostAmit${count.index}"
   create_vnic_details {
     assign_public_ip = true
-    subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaa74trcuememyseily3jm5ltzbf7kr4nuv5z3rfaxfiivpln3jeefa"
+    // For AmitChildCompartment
+    // subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaa74trcuememyseily3jm5ltzbf7kr4nuv5z3rfaxfiivpln3jeefa"
+    //For Intellibliss Public Subnet
+    subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaamfuaw2esu2d5tv4n76hzetytv5v2qkf2nospjppjhm42rgntknba"
   }
  # metadata = {
   #ssh_authorized_keys = file(var.ssh_public_key)
