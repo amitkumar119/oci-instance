a770feb79090ce4dff7ff9af5a2b3f37d6f4cfb5
amitkumar119 <35453180+amitkumar119@users.noreply.github.com>
2022-02-04 12:46:07 +0530

    Update instance.tf
    For partner Tenancy

--- instance.tf
+++ instance.tf
@@ -2,38 +2,42 @@
 provider "oci" {
    region = "us-ashburn-1"
    //alias = "phx"
-   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaauuao4pnrffvzgxylvbobagcyo35qsu6z4bqdvjcla4czevhriqvq"
-   user_ocid = "ocid1.user.oc1..aaaaaaaawimuaayeafktaqctwt4twmobbka5awtsepkd4rvss4mtvykxg3pa"
-   fingerprint = "9c:ed:78:5d:da:35:17:05:0a:6b:93:be:b0:84:38:5c"
-   private_key_path = "./amitTestPrivate.pem"
+   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa4mzf57gl7afpkuhti3rq76gie762ciyfwuvu6xn4tcxndj7zhi3q"
+   user_ocid = "ocid1.user.oc1..aaaaaaaazqp7tubplumg7ej2lfcdwwwgifjywwww4ct33ntv3fsixkbtybpa"
+   fingerprint = "8e:95:5f:54:c0:f5:28:b3:fe:9b:0b:1f:a8:e7:1e:6b"
+   private_key_path = "./amitTestPrivatePartner.pem"
 }
 
 
 resource "oci_core_instance" "testhostAmit" {
   # Required
   count = 1
-  availability_domain = "ubQd:US-ASHBURN-AD-2"
+  availability_domain = "IYfK:US-ASHBURN-AD-1"
   // For Compartment = AmitChildCompartment 
   //compartment_id      = "ocid1.compartment.oc1..aaaaaaaaywbpujwoxt7wpxvxawpv2xneun36ccmwk4baal7gpjmsswosmrwq"
   // For Compartment = oci-intelibliss-test
-  compartment_id =  "ocid1.compartment.oc1..aaaaaaaammdmuwe7hgei2iu4lar6rtg7o4gmvnp572lfxvgwo5xky4z6ajwa"
+  // compartment_id =  "ocid1.compartment.oc1..aaaaaaaammdmuwe7hgei2iu4lar6rtg7o4gmvnp572lfxvgwo5xky4z6ajwa"
+  // The below compartment is for Partner Tenancy
+   compartment_id =  "ocid1.compartment.oc1..aaaaaaaad4r5xrowh7vfojhsifwew473fwruqd5asuppfwzqytoyml6lizda"
   shape               = "VM.Standard.E2.1.Micro"
   shape_config {
     memory_in_gbs = 1
     ocpus         = 1
   }
   source_details {
-    source_id   = "ocid1.image.oc1.iad.aaaaaaaagubx53kzend5acdvvayliuna2fs623ytlwalehfte7z2zdq7f6ya"
+    source_id   = "ocid1.image.oc1.iad.aaaaaaaaffh3tppq63kph77k3plaaktuxiu43vnz2y5oefkec37kwh7oomea"
     source_type = "image"
   }
   # Optional
-  display_name = "testhostAmit${count.index}"
+  display_name = "testhostAmitPartner${count.index}"
   create_vnic_details {
     assign_public_ip = true
     // For AmitChildCompartment
     // subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaa74trcuememyseily3jm5ltzbf7kr4nuv5z3rfaxfiivpln3jeefa"
     //For Intellibliss Public Subnet
-    subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaamfuaw2esu2d5tv4n76hzetytv5v2qkf2nospjppjhm42rgntknba"
+    //subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaamfuaw2esu2d5tv4n76hzetytv5v2qkf2nospjppjhm42rgntknba"
+    //For Partner tenancy Subnet.     
+    subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaalmeu2nnplhqmxer74rkyfnpptcazk427fkygv2tk6eyfdc5fiicq"
   }
  # metadata = {
   #ssh_authorized_keys = file(var.ssh_public_key)
